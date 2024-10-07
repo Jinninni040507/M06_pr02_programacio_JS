cda11b15e0fa9f0fac5345d50bff5015e4ddebda
Jinninni040507 <moni.oliveras05@gmail.com>
2024-10-07 19:35:53 +0200

    #1 exercici exemple done

---
+++ Exercici 01/Exercici01.js
@@ -0,0 +1,11 @@
+const valorMin = document.getElementById("valor_min");
+const ampladaTotal = document.getElementById("amplada_total");
+const ampladaInterna = document.getElementById("amplada_interna");
+const titol = document.getElementById("titol");
+const horaActual = document.getElementById("hora_actual");
+
+valorMin.textContent = Number.MIN_VALUE;
+ampladaTotal.textContent = screen.width;
+ampladaInterna.textContent = window.innerWidth;
+titol.textContent = document.title;
+horaActual.textContent = new Date().getHours();
